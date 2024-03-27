da0699eb8f49f811b10e83da69c1f1e6cc2435f4
utopusc <burakcanoztruk@gmail.com>
2024-03-27 03:27:20 +0000

    main

--- src/config.js
+++ src/config.js
@@ -13,7 +13,8 @@ export const config = {
     version: process.env.NEXT_PUBLIC_SITE_VERSION || '0.0.1',
   },
   logLevel: process.env.NEXT_PUBLIC_LOG_LEVEL || LogLevel.ALL,
-  auth: { strategy: process.env.NEXT_PUBLIC_AUTH_STRATEGY || AuthStrategy.CUSTOM },
+  auth: { strategy: AuthStrategy.AUTH0,
+  },
   auth0: {
     secret: process.env.AUTH0_SECRET,
     baseUrl: process.env.AUTH0_BASE_URL,
@@ -21,20 +22,6 @@ export const config = {
     clientId: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
   },
-  cognito: {
-    identityPoolId: process.env.NEXT_PUBLIC_COGNITO_IDENTITY_POOL_ID,
-    userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID,
-    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
-  },
-  firebase: {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  },
-  supabase: { url: process.env.NEXT_PUBLIC_SUPABASE_URL, anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY },
   mapbox: { apiKey: process.env.NEXT_PUBLIC_MAPBOX_API_KEY },
   gtm: { id: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID },
 };
